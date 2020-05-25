758d44c109cc8b52ebe3336e5ccccc04f6d21e88
Heidi <Caughley.h@rangiruru.school.nz>
2020-05-25 15:06:58 +1200

    Version 1 - Added checkboxes and when some or all are clicked, a password will be generated
    Added checkboxes and when some or all are clicked, a password will be generated. I tested on expected inputs upperCase and then generating, lowercase and then generating, upperCase and lowerCase together, upperCase, special characters and lowercase together and then generating. Special Characters and then generating, numbers and then generating, special characters and then generating. I also tested on boundary unputs selecting all 4 options and made sure that the generator still workied. I also tested invalid input of no input and a message was generated to ask the user to please select at least one checkbox. All tests worked as expected

--- src/index.svelte
+++ src/index.svelte
@@ -1,3 +1,108 @@
-<h1>Svelte</h1>
+<script>
+  let lengthWanted = "";
+  let letters = false;
+  let numbers = false;
+  let specialCharacters = false;
+  let upperCase = false;
+  let lowerCase = false;
+  let finalPassword = "";
+  let lengthOfPassword = 0;
 
-<p>Welcome to coding with Svelte!</p>
+  function checkLetters() {
+    letters = true;
+  }
+
+  function checkNumbers() {
+    numbers = true;
+  }
+
+  function checkSpecialCharacters() {
+    specialCharacters = true;
+  }
+
+  function checkUpperCase() {
+    upperCase = true;
+  }
+  function checkLowerCase() {
+    lowerCase = true;
+  }
+
+  function generatePassword() {
+    lengthOfPassword++;
+    if (lowerCase) {
+      let randomNumberLowerCase = Math.floor(Math.random() * 26) + 97;
+      finalPassword += String.fromCharCode(randomNumberLowerCase);
+      console.log(randomNumberLowerCase);
+
+    }
+    if (numbers) {
+      let randomNumberNumbers = Math.floor(Math.random() * 10) + 48;
+      finalPassword += String.fromCharCode(randomNumberNumbers);
+      console.log(randomNumberNumbers);
+    }
+    if (specialCharacters) {
+      let randomNumberSpecialCharacters = Math.floor(Math.random() * 4) + 35;
+      finalPassword += String.fromCharCode(randomNumberSpecialCharacters);
+      console.log(randomNumberSpecialCharacters);
+    }
+    if (upperCase) {
+      let randomNumberUpperCase = Math.floor(Math.random() * 26) + 65;
+      finalPassword += String.fromCharCode(randomNumberUpperCase);
+      console.log(randomNumberSpecialCharacters)
+    }
+
+   if (upperCase === false && specialCharacters === false && numbers === false && upperCase === false && lowerCase === false)
+   finalPassword = "Please select a checkbox"
+    return;
+  }
+</script>
+
+<section class="section content">
+  <h1>Password Generator</h1>
+  <label>
+    Length:{lengthWanted}
+    <input
+      type="range"
+      min="0"
+      max="20"
+      id="passwordlength"
+      bind:value={lengthWanted} />
+  </label>
+
+  <label>
+    Numbers:
+    <input
+      type="checkbox"
+      id="characters"
+      on:click={checkNumbers}
+      bind:value={numbers} />
+    Special Characters:
+    <input
+      type="checkbox"
+      id="characters"
+      on:click={checkSpecialCharacters}
+      bind:value={specialCharacters} />
+  </label>
+  <label>
+    Uppercase letters
+    <input
+      type="checkbox"
+      id="uppercase"
+      on:click={checkUpperCase}
+      bind:value={upperCase} />
+    Lowercase letters
+    <input
+      type="checkbox"
+      id="uppercase"
+      on:click={checkLowerCase}
+      bind:value={lowerCase} />
+
+  </label>
+  <button
+    class="button is-success is-outlined"
+    id="generatepassword"
+    on:click={generatePassword}>
+    Generate Password
+  </button>
+  {finalPassword}
+</section>
